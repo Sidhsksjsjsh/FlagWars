999df9c9eaa0cf7573ba510011fe7d74d25df180
Ambatukam <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-06-02 21:15:04 +0900

    Update Script.lua

--- Roblox/Script.lua
+++ Roblox/Script.lua
@@ -398,6 +398,7 @@ Tab1:CreateButton("Bring All ( Client Sided )", function()
     end
 end)
 
+--[[
 Tab2:CreateButton("Gun Mods", function()
       notify.new("warning","Cheat Enabled","reset to work")
 local mt = getrawmetatable(game)
@@ -415,6 +416,47 @@ local Mods = {
     ["ShotCooldown"] = 0,
     ["CurrentAmmo"] = math.huge,
     ["BulletSpeed"] = math.huge,
+    ["FireMode"] = "Automatic",
+    ["GravityFactor"] = 0,
+    ["MaxSpread"] = 0,
+    ["MinSpread"] = 0,
+    ["HitDamage"] = 9e9,
+    ["NumProjectiles"] = 1,
+    -- Melee (Shovel, Sword, etc)
+    ["Cooldown"] = 0,
+    ["Value"] = 0,
+    ["DirtDamage"] = 9e9,
+    ["SwingCooldown"] = 0,
+    ["HitRate"] = 0,
+}
+
+mt.__index = function(a,b)
+    if Mods[tostring(a)] then
+        if tostring(b) == "Value" then
+            return Mods[tostring(a)]
+        end
+    end
+    return index(a,b)
+end
+end)
+--]]
+
+Tab2:CreateButton("inf bullet and no recoil", function()
+      notify.new("warning","Cheat Enabled","reset to work")
+local mt = getrawmetatable(game)
+setreadonly(mt, false)
+local index = mt.__index
+
+local Mods = {
+    -- Guns (Spas, Sniper, etc)
+    ["RecoilMax"] = 0,
+    ["RecoilMin"] = 0,
+    ["MuzzleFlashSize0"] = 0,
+    ["MuzzleFlashSize1"] = 0,
+    ["FlashBrightness"] = 0,
+    ["AmmoCapacity"] = math.huge,
+    ["ShotCooldown"] = 0,
+    ["CurrentAmmo"] = math.huge,
     ["FireMode"] = "Automatic",
     ["GravityFactor"] = 0,
     ["MaxSpread"] = 0,
