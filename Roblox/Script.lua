655f9986646bbc02af41d5d75756253a979b7092
Ambatukam <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-06-04 07:10:03 +0900

    Update Script.lua

--- Roblox/Script.lua
+++ Roblox/Script.lua
@@ -25,28 +25,19 @@ Tab1:CreateButton("Silent Aim V1", function()
             ["fov"] = 100,
             ["hitpart"] = "Head",
             ["circlevis"] = true,
-            ["wallbang"] = false,
+            ["wallbang"] = true,
             ["circcolor"] = Color3.fromRGB(228, 9, 191)
         }
     }
 
 
-    local CircleInline = Drawing.new("Circle")
-    local CircleOutline = Drawing.new("Circle")
-    CircleInline.Radius = getgenv().GameSettings.SilentAim.fov
-    CircleInline.Thickness = 2
-    CircleInline.Position = Vector2.new(currCamera.ViewportSize.X / 2, currCamera.ViewportSize.Y / 2)
-    CircleInline.Transparency = 1
-    CircleInline.Color = getgenv().GameSettings.SilentAim.circcolor
-    CircleInline.Visible = getgenv().GameSettings.SilentAim.circlevis
-    CircleInline.ZIndex = 2
-    CircleOutline.Radius = getgenv().GameSettings.SilentAim.fov
-    CircleOutline.Thickness = 4
-    CircleOutline.Position = Vector2.new(currCamera.ViewportSize.X / 2, currCamera.ViewportSize.Y / 2)
-    CircleOutline.Transparency = 1
-    CircleOutline.Color = Color3.new()
-    CircleOutline.Visible = getgenv().GameSettings.SilentAim.circlevis
-    CircleOutline.ZIndex = 1
+    local FOVring = Drawing.new("Circle")
+FOVring.Visible = true
+FOVring.Thickness = 1.5
+FOVring.Radius = 100
+FOVring.Transparency = 1
+FOVring.Color = Color3.fromRGB(228, 9, 191)
+FOVring.Position = currCamera.ViewportSize/2
 
 
     function isSameTeam(player)
@@ -114,7 +105,7 @@ Tab1:CreateButton("Silent Aim V1", function()
                     target = isClosestPlayer()
                     if target then
                         Args[2]["part"] = target.Character[getgenv().GameSettings.SilentAim.hitpart]
-                        return self:FireServer(self, unpack(Args))
+                        return self.FireServer(self, unpack(Args))
                     end
                 end
                 return oldNamecall(self, ...)
@@ -122,6 +113,65 @@ Tab1:CreateButton("Silent Aim V1", function()
         )
 end)
 
+--[[
+local teamCheck = false
+local fov = 150
+local smoothing = 1
+
+local RunService = game:GetService("RunService")
+
+local FOVring = Drawing.new("Circle")
+FOVring.Visible = true
+FOVring.Thickness = 1.5
+FOVring.Radius = fov
+FOVring.Transparency = 1
+FOVring.Color = Color3.fromRGB(255, 128, 128)
+FOVring.Position = workspace.CurrentCamera.ViewportSize/2
+
+local function getClosest(cframe)
+   local ray = Ray.new(cframe.Position, cframe.LookVector).Unit
+   
+   local target = nil
+   local mag = math.huge
+   
+   for i,v in pairs(game.Players:GetPlayers()) do
+       if v.Character and v.Character:FindFirstChild("Head") and v.Character:FindFirstChild("Humanoid") and v.Character:FindFirstChild("HumanoidRootPart") and v ~= game.Players.LocalPlayer and (v.Team ~= game.Players.LocalPlayer.Team or (not teamCheck)) then
+           local magBuf = (v.Character.Head.Position - ray:ClosestPoint(v.Character.Head.Position)).Magnitude
+           
+           if magBuf < mag then
+               mag = magBuf
+               target = v
+           end
+       end
+   end
+   
+   return target
+end
+
+loop = RunService.RenderStepped:Connect(function()
+   local UserInputService = game:GetService("UserInputService")
+   local pressed = --[[UserInputService:IsKeyDown(Enum.KeyCode.E) UserInputService:IsMouseButtonPressed(Enum.UserInputType.MouseButton2) --Enum.UserInputType.MouseButton2
+   local localPlay = game.Players.localPlayer.Character
+   local cam = workspace.CurrentCamera
+   local zz = workspace.CurrentCamera.ViewportSize/2
+   
+   if pressed then
+       local Line = Drawing.new("Line")
+       local curTar = getClosest(cam.CFrame)
+       local ssHeadPoint = cam:WorldToScreenPoint(curTar.Character.Head.Position)
+       ssHeadPoint = Vector2.new(ssHeadPoint.X, ssHeadPoint.Y)
+       if (ssHeadPoint - zz).Magnitude < fov then
+           workspace.CurrentCamera.CFrame = workspace.CurrentCamera.CFrame:Lerp(CFrame.new(cam.CFrame.Position, curTar.Character.Head.Position), smoothing)
+       end
+   end
+   
+   if UserInputService:IsKeyDown(Enum.KeyCode.Delete) then
+       loop:Disconnect()
+       FOVring:Remove()
+   end
+end)
+end)
+--]]
 Tab1:CreateButton("Silent Aim V2", function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/kalasthrowaway/stuff/main/flagwars.lua"))()
         notify.new("success","Silent Aim","Silent Aim Enabled, bullets will follow the movement of the enemy")
