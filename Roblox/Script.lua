68830a2a76135bb9bcf0c5005c87563abfac32df
Ambatukam <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-06-05 09:53:57 +0900

    Update Script.lua

--- Roblox/Script.lua
+++ Roblox/Script.lua
@@ -1,14 +1,40 @@
-local Library = 
-loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
-local Window_1 = Library:NewWindow("Flag Wars")
+local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/NMEHkVTb"))()
 
-local Tab1 = Window_1:NewSection("Main")
-local Tab2 = Window_1:NewSection("Gun Config")
-local Tab3 = Window_1:NewSection("Flag")
+local Window = OrionLib:MakeWindow({Name = "VIP Turtle Hub V3", HidePremium = false, SaveConfig = true, ConfigFolder = "TurtleFi"})
 
-local notify = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua"))()
+local Tab = Window:MakeTab({
+Name = "Main",
+Icon = "rbxassetid://4483345998",
+PremiumOnly = false
+})
 
-Tab1:CreateButton("Silent Aim V1", function()
+local Tab2 = Window:MakeTab({
+Name = "Modded",
+Icon = "rbxassetid://4483345998",
+PremiumOnly = false
+})
+
+local Tab3 = Window:MakeTab({
+Name = "Gun Mod",
+Icon = "rbxassetid://4483345998",
+PremiumOnly = false
+})
+
+local Tab4 = Window:MakeTab({
+Name = "Flag",
+Icon = "rbxassetid://4483345998",
+PremiumOnly = false
+})
+
+local Tab5 = Window:MakeTab({
+Name = "Character",
+Icon = "rbxassetid://4483345998",
+PremiumOnly = false
+})
+
+Tab:AddButton({
+Name = "Silent Aim V1",
+Callback = function()
     local currPlayer = game:GetService('Players').LocalPlayer
     local servPlayer = game:GetService('Players')
 
@@ -111,7 +137,8 @@ FOVring.Position = currCamera.ViewportSize/2
                 return oldNamecall(self, ...)
             end
         )
-end)
+end
+})
 
 --[[
 local teamCheck = false
@@ -172,12 +199,17 @@ loop = RunService.RenderStepped:Connect(function()
 end)
 end)
 --]]
-Tab1:CreateButton("Silent Aim V2", function()
+Tab:AddButton({
+Name = "Silent Aim V2",
+Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/kalasthrowaway/stuff/main/flagwars.lua"))()
         notify.new("success","Silent Aim","Silent Aim Enabled, bullets will follow the movement of the enemy")
-end)
+end
+})
 
-Tab1:CreateButton("Trigger Bot", function()
+Tab:AddButton({
+Name = "Trigger Bot",
+Callback = function()
     local currPlayer = game:GetService('Players').LocalPlayer
     local servPlayer = game:GetService('Players')
     local currPlayerCharacter = currPlayer.Character or currPlayer.CharacterAdded:wait()
@@ -202,9 +234,12 @@ Tab1:CreateButton("Trigger Bot", function()
     RunService.Stepped:Connect(function()
     trigBot()
     end)
-end)
+end
+})
 
-Tab1:CreateButton("Kill All ( Sniper )", function()
+Tab:AddButton({
+Name = "Kill All (Sniper)",
+Callback = function()
     local servPlayer = game:GetService("Players")
     local currPlayer = game:GetService('Players').LocalPlayer
     
@@ -245,52 +280,12 @@ Tab1:CreateButton("Kill All ( Sniper )", function()
     end
         
    notify.new("success","Cheat Enabled ( Sniper )","start killing all enemies")
-end)
+end
+})
 
-Tab1:CreateButton("Kill All ( M4A1 )", function()
-    local servPlayer = game:GetService("Players")
-    local currPlayer = game:GetService('Players').LocalPlayer
-    
-    function getEquippedWeapon(player)
-        local char = player.Character
-        local weapon = char and char:FindFirstChildWhichIsA("Tool")
-    
-        if weapon ~= nil then
-            return weapon.Name
-        else
-            return "Holstered"
-        end
-    end
-    
-    local currWeapon = getEquippedWeapon(currPlayer)
-    
-    for _, v in pairs(servPlayer:GetPlayers()) do
-        if v.Name ~= currPlayer.Name then
-            task.wait()
-    local args = {
-    [1] = game:GetService("Players").LocalPlayer.Character.M4A1,
-    [2] = {
-        ["p"] = Vector3.new(519.5029296875, 44.33285903930664, 144.1508331298828),
-        ["pid"] = 1,
-        ["part"] = v.Character.Head,
-        ["d"] = 42.01991271972656,
-        ["maxDist"] = 40.19593811035156,
-        ["h"] = v.Character.Humanoid,
-        ["m"] = Enum.Material.SmoothPlastic,
-        ["sid"] = 5,
-        ["t"] = 0.03969588453359542,
-        ["n"] = Vector3.new(-0.12755782902240753, -0.40483540296554565, -0.9054485559463501)
-    }
-}
-
-game:GetService("ReplicatedStorage").WeaponsSystem.Network.WeaponHit:FireServer(unpack(args))
-    end
-    end
-        
-   notify.new("success","Cheat Enabled ( M4A1 )","start killing all enemies")
-end)
-
-Tab1:CreateButton("Hitbox Expander", function()
+Tab:AddButton({
+Name = "Hitbox Expander",
+Callback = function()
     local currPlayer = game:GetService("Players").LocalPlayer
     local servPlayer = game:GetService("Players")
     local RunService = game:GetService("RunService")
@@ -339,9 +334,12 @@ Tab1:CreateButton("Hitbox Expander", function()
         end
     end
     end)
-end)
+end
+    })
 
-Tab1:CreateButton("ESP", function()
+Tab:AddButton({
+Name = "ESP",
+Callback = function()
     local currPlayer = game:GetService("Players").LocalPlayer
     local servPlayer = game:GetService("Players")
     local RunService = game:GetService("RunService")
@@ -422,9 +420,12 @@ Tab1:CreateButton("ESP", function()
     doESP()
     end
     )
-end)
+end
+    })
 
-Tab1:CreateButton("Bring All ( Client Sided )", function()
+Tab:AddButton({
+Name = "Bring All ( Client Sided )",
+Callback = function()
     local currPlayer = game:GetService("Players").LocalPlayer
     local servPlayer = game:GetService("Players")
     
@@ -446,7 +447,8 @@ Tab1:CreateButton("Bring All ( Client Sided )", function()
         end
     end
     end
-end)
+end
+    })
 
 --[[
 Tab2:CreateButton("Gun Mods", function()
@@ -491,7 +493,9 @@ end
 end)
 --]]
 
-Tab2:CreateButton("Gun Mod", function()
+Tab3:AddButton({
+Name = "Rapid Fire & Infinite Ammo",
+Callback = function()
       notify.new("warning","Cheat Enabled","reset to work")
 local mt = getrawmetatable(game)
 setreadonly(mt, false)
@@ -529,9 +533,12 @@ mt.__index = function(a,b)
     end
     return index(a,b)
 end
-end)
+end
+    })
 
-Tab3:CreateButton("Teleport to flag", function()
+Tab4:AddButton({
+Name = "Teleport to flag (Kick Bypass)",
+Callback = function()
 for i,v in pairs(game.Workspace:GetDescendants()) do
         if v:IsA("BasePart") then
             if v.Name:lower() == "flag" then
@@ -539,4 +546,369 @@ for i,v in pairs(game.Workspace:GetDescendants()) do
             end
         end
     end
+end
+    })
+
+
+local Workspace = game:GetService("Workspace")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local Camera = Workspace.CurrentCamera
+local UserInputService = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
+local Mouse = LocalPlayer:GetMouse()
+
+
+_G.Raven = {
+    Silent = false,
+    TeamC = false,
+    FovC = false,
+    Wallbang = false
+}
+
+Tab2:AddToggle({
+Name = "Bullet Track",
+Default = false,
+Callback = function(Value)
+     _G.Raven.SilentC = Value
+end
+})
+
+Tab2:AddToggle({
+Name = "Team Check",
+Default = false,
+Callback = function(Value)
+     _G.Raven.TeamC = Value
+end
+})
+
+Tab2:AddToggle({
+Name = "Fov Circle",
+Default = false,
+Callback = function(Value)
+     _G.Raven.FovC = Value
+end
+})
+
+Tab2:AddToggle({
+Name = "Wallhack",
+Default = false,
+Callback = function(Value)
+     _G.Raven.Wallbang = Value
+end
+})
+
+
+_G.Slider = {
+    FovRadius = 0,
+    FovSides = 0,
+    FovT = 0,
+    FovColor = Color3.new(1, 1, 0),
+    SilentHitbox = "",
+    SilentDirec = 0
+}
+
+Tab2:AddSlider({
+Name = "Fov Size",
+Min = 5,
+Max = 500,
+Default = 90,
+Color = Color3.fromRGB(255,255,255),
+Increment = 1,
+ValueName = "Size",
+Callback = function(Value)
+      _G.Slider.FovRadius = Value
+end
+})
+
+Tab2:AddSlider({
+Name = "Fov Sides",
+Min = 5,
+Max = 64,
+Default = 64,
+Color = Color3.fromRGB(255,255,255),
+Increment = 1,
+ValueName = "Sides",
+Callback = function(Value)
+      _G.Slider.FovSides = Value
+end
+})
+
+Tab2:AddSlider({
+Name = "Fov Circle Thickness",
+Min = 0,
+Max = 9,
+Default = 5,
+Color = Color3.fromRGB(255,255,255),
+Increment = 1,
+ValueName = "Thickness",
+Callback = function(Value)
+      _G.Slider.FovT = Value
+end
+})
+
+Tab2:AddColorpicker({
+Name = "Fov Color",
+Default = Color3.fromRGB(255, 0, 0),
+Callback = function(Value)
+    _G.Slider.FovColor = Color3.new(Value)
+end  
+})
+
+Tab:AddDropdown({
+Name = "Bullet Target",
+Default = "",
+Options = {'Head', 'UpperTorso', 'LowerTorso'},
+Callback = function(Value)
+    _G.Slider.SilentHitbox = Value
+end    
+})
+
+Tab2:AddSlider({
+Name = "Bullet Prediction",
+Min = 0,
+Max = 100,
+Default = 50,
+Color = Color3.fromRGB(255,255,255),
+Increment = 1,
+ValueName = "% Prediction",
+Callback = function(Value)
+      _G.Slider.SilentDirec = Value
+end
+})
+
+local silentFovRad = _G.Slider.FovRadius
+local silentFovColor = _G.Slider.FovColor
+local silentFovVisible = _G.Raven.FovC
+local silentNumSides = _G.Slider.FovSides
+local silentThickness = _G.Slider.FovT
+local silentAimPart = _G.Slider.SilentDirec
+
+
+local SilentFovCircle = Drawing.new('Circle')
+SilentFovCircle.Position = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2)
+SilentFovCircle.Radius = silentFovRad
+SilentFovCircle.Color = silentFovColor
+SilentFovCircle.Filled = false
+SilentFovCircle.Visible = silentFovVisible
+SilentFovCircle.Transparency = 0.5
+SilentFovCircle.NumSides = silentNumSides
+SilentFovCircle.Thickness = silentThickness
+
+getgenv().Target = nil
+--[[
+_G.Raven = {
+    Silent = false,
+    TeamC = false,
+    FovC = false,
+    Wallbang = false
+}
+--]]
+local function silentClosetPlayer()
+    local MaximumDistance = _G.Slider.FovRadius
+
+    for _, v in next, Players:GetPlayers() do
+        if v.Name ~= LocalPlayer.Name then
+            if _G.Raven.TeamC == true then
+                if v.Team ~= LocalPlayer.Team then
+                    if v.Character ~= nil then
+                        if v.Character:FindFirstChild("HumanoidRootPart") then
+                            if v.Character:FindFirstChild("Humanoid") and v.Character:FindFirstChild("Humanoid").Health ~= 0 then
+                                local ScreenPoint = Camera:WorldToScreenPoint(v.Character:WaitForChild("HumanoidRootPart", math.huge).Position)
+                                local VectorDistance = (Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2) - Vector2.new(ScreenPoint.X, ScreenPoint.Y)).Magnitude
+                    
+                                if VectorDistance < MaximumDistance then
+                                    getgenv().Target = v
+                                end
+                            end
+                        end
+                    end
+                end
+            else
+                if v.Character ~= nil then
+                    if v.Character:FindFirstChild("HumanoidRootPart") then
+                        if v.Character:FindFirstChild("Humanoid") and v.Character:FindFirstChild("Humanoid").Health ~= 0 then
+                            local ScreenPoint = Camera:WorldToScreenPoint(v.Character:WaitForChild("HumanoidRootPart", math.huge).Position)
+                            local VectorDistance = (Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2) - Vector2.new(ScreenPoint.X, ScreenPoint.Y)).Magnitude
+                            
+                            if VectorDistance < MaximumDistance then
+                                getgenv().Target = v
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+
+RunService.RenderStepped:Connect(function()
+    SilentFovCircle.Position = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2)
+    SilentFovCircle.Radius = _G.Slider.FovRadius
+    SilentFovCircle.Filled = false
+    SilentFovCircle.Color = _G.Slider.FovColor
+    SilentFovCircle.Visible = true
+    SilentFovCircle.Transparency = 0.5
+    SilentFovCircle.NumSides = _G.Slider.FovSides
+    SilentFovCircle.Thickness = _G.Slider.FovT
+    silentClosetPlayer()
+    if _G.Raven.FovC == false or _G.Raven.Silent == false then
+        SilentFovCircle.Visible = false
+    end
+end)
+
+
+--[[
+    ######### ESP
+]]
+
+--[[
+local ESP = loadstring(game:HttpGet("https://kiriot22.com/releases/ESP.lua"))()
+ESP:Toggle(true)
+ESP.Color = Color3.new(1,1,1)
+
+ESPContainer:AddToggle('boxesTGL', {Text = 'Boxes', Default = false, Tooltip = 'Toggle the Boxes ESP'})
+ESPContainer:AddToggle('tracersTGL', {Text = 'Tracers', Default = false, Tooltip = 'Toggle the Tracers ESP'})
+ESPContainer:AddToggle('namesTGL', {Text = 'Names', Default = false, Tooltip = 'Toggle the Names ESP'})
+_G.esp = {
+    Boxes = false,
+    Tracer = false,
+    Name = false
+    team = false
+    color = false
+    BoxesC = false
+    espcolor = Color3.new(1, 1, 0)
+}
+
+ESPConfiguration:AddToggle('teamESPTgl', {Text = 'Team ESP', Default = false, Tooltip = 'Choose if you want ESP on your teammates aswell!'})
+ESPConfiguration:AddToggle('teamcolorTgl', {Text = 'Use team color', Default = true, Tooltip = 'Choose if you want to use the team color or not.'})
+ESPConfiguration:AddToggle('boxesCameraTgl', {Text = 'Boxes Face Camera', Default = true, Tooltip = 'Choose if you want the boxes to face the camera or not.'})
+ESPConfiguration:AddLabel('ESP Color'):AddColorPicker('ESP_Color', {Default = Color3.new(1, 1, 0)})
+
+
+Toggles.boxesTGL:OnChanged(function()
+    ESP.Boxes = Toggles.boxesTGL.Value
+end)
+
+Toggles.tracersTGL:OnChanged(function()
+    ESP.Tracers = Toggles.tracersTGL.Value
+end)
+
+Toggles.namesTGL:OnChanged(function()
+    ESP.Names = Toggles.namesTGL.Value
+end)
+
+
+Toggles.teamcolorTgl:OnChanged(function()
+    ESP.TeamColor = Toggles.teamcolorTgl.Value
+end)
+
+Toggles.boxesCameraTgl:OnChanged(function()
+    ESP.FaceCamera = Toggles.boxesCameraTgl.Value
+end)
+
+Options.ESP_Color:OnChanged(function()
+    ESP.Color = Options.ESP_Color.Value
+end)
+
+Toggles.teamESPTgl:OnChanged(function()
+    ESP.TeamMates = Toggles.teamESPTgl.Value
+end)
+
+
+--[[
+    ########## MISC ###########
+
+
+
+
+local MiscLeftGroupBox = Tabs.MiscTab:AddLeftGroupbox("Misc Toggles")
+local MiscRightGroupBox = Tabs.MiscTab:AddRightGroupbox("Misc Configuration")
+
+MiscLeftGroupBox:AddToggle('WSToggle', {Text = "Walkspeed", Default = false, Tooltip = 'Modify your walkspeed.'})
+MiscLeftGroupBox:AddToggle('JPToggle', {Text = "Jump Power", Default = false, Tooltip = 'Modify your jump power!' })
+MiscLeftGroupBox:AddToggle('GravityToggle', {Text = 'Gravity Changer', Default = false, Tooltip = 'Changes the game gravity',})
+MiscLeftGroupBox:AddToggle('infJump', {Text = 'Infinite Jump', Default = false})
+
+
+MiscRightGroupBox:AddSlider('WSlider', {Text = 'WalkSpeed', Default = 24, Min = 5, Max = 1000, Rounding = 0, Compact = false,})
+MiscRightGroupBox:AddSlider('JPSlider', {Text = 'JumpPower', Default = 24, Min = 5, Max = 1000, Rounding = 0, Compact = false})
+MiscRightGroupBox:AddSlider('GravSlider', {Text = 'Gravity', Default = 196, Min = 5, Max = 1000, Rounding = 0, Compact = false})
+
+
+
+Toggles.WSToggle:OnChanged(function()
+    if Toggles.WSToggle.Value == false then
+        LocalPlayer.Character.Humanoid.WalkSpeed = 16
+    end
+end)
+
+Options.WSlider:OnChanged(function()
+    if Toggles.WSToggle.Value == false then return end
+    LocalPlayer.Character.Humanoid.WalkSpeed = Options.WSlider.Value
+end)
+
+Toggles.JPToggle:OnChanged(function()
+    if Toggles.JPToggle.Value == false then
+        LocalPlayer.Character.Humanoid.JumpPower = 55
+    end
+end)
+
+Options.JPSlider:OnChanged(function()
+    if Toggles.JPToggle.Value == false then return end
+    LocalPlayer.Character.Humanoid.JumpPower = Options.JPSlider.Value
+end)
+
+
+Toggles.GravityToggle:OnChanged(function()
+    if Toggles.GravityToggle.Value == false then
+        game.Workspace.Gravity = 196.2
+    end
+end)
+
+Options.GravSlider:OnChanged(function()
+    if Toggles.GravityToggle.Value == false then return end
+    if Toggles.GravityToggle.Value == true then
+        game.Workspace.Gravity = Options.GravSlider.Value
+    end
+end)
+
+UserInputService.JumpRequest:connect(function()
+	if Toggles.infJump.Value then
+		LocalPlayer.Character:FindFirstChildOfClass('Humanoid'):ChangeState("Jumping")
+	end
+end)
+
+
+local MenuGroup = Tabs['UI Settings']:AddLeftGroupbox('Menu')
+
+MenuGroup:AddButton('Unload', function() Library:Unload() end)
+MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = true, Text = 'Menu keybind' }) 
+
+Library.ToggleKeybind = Options.MenuKeybind
+
+ThemeManager:SetLibrary(Library)
+ThemeManager:SetFolder('MyScriptHub')
+ThemeManager:ApplyToTab(Tabs['UI Settings'])
+--]]
+
+--[[
+    ###### Hooks
+]]
+
+local old
+old = hookmetamethod(game, "__namecall", function(Self, ...)
+    local Args = {...}
+    if Self.Name == "Shoot" and getgenv().Target ~= nil and _G.Ravel.Silent == true then
+        local base = getgenv().Target.Character[_G.Slider.SilentHitbox]
+        local velocity = base.AssemblyLinearVelocity
+        local aimpos = base.Position + (velocity * Vector3.new(_G.Slider.SilentDirec / 200, 0, _G.Slider.SilentDirec / 200))
+        Args[3] = aimpos
+        if _G.Ravel.Wallbang == true then
+            Args[2] = getgenv().Target.Character[_G.Slider.SilentHitbox].Position
+        end
+        return old(Self, unpack(Args))
+    end
+    return old(Self, unpack(Args))
 end)
